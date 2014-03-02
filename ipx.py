c2bd4fb2f57a9c9509ac007b3dcb99d6f2a216e3
KennethD96 <Kenneth.dahle.1996@gmail.com>
2014-03-02 21:23:36 +0100

    Added emucontrol class

--- ipx.py
+++ ipx.py
@@ -1,11 +1,25 @@
+#encoding: utf-8
 from __future__ import division
 
+from bones.bot import Module
 import bones.bot
 import bones.event
-from twisted.internet import reactor
+from keys import PressKey, ReleaseKey
 
+from subprocess import Popen, PIPE
+from twisted.internet import reactor
+from os import path
 import time
-from IPX.keys import PressKey, ReleaseKey
+import re
+
+# Config
+
+default_emu = "bgb.exe"
+default_rom = "Pokemon Yellow.gb"
+load_at_startup = False
+input_enabled = True
+
+mod_admins = ["KennethD", "_404`d"]
 
 keys = {
     "down": 0x28,
@@ -20,10 +34,109 @@ keys = {
     "select": 0x08,
 }
 keyDelay = (1000/59.97)/1000
-    
-class IRCPlaysPokemon(bones.bot.Module):
+
+##########################################
+
+module_path = path.dirname(__file__)
+emu_path = path.join(module_path, "emulators")
+rom_path = path.join(module_path, "roms")
+active_emu = default_emu
+active_rom = default_rom
+
+class emucontrol(Module):
+    def __init__(self, *args, **kwargs):
+        Module.__init__(self, *args, **kwargs)
+        self.active_emu = path.join(emu_path, active_emu)
+        self.active_rom = path.join(rom_path, active_rom)
+        if load_at_startup:
+            self.em = Popen([self.active_emu, self.active_rom])
+        self.em = None
+
+    def isrunning(proc):
+        try:
+            return proc.poll()
+        except:
+            return 0
+
+    @bones.event.handler(trigger="emustart")
+    def emustart(self, event):
+        if event.user.nickname in mod_admins:
+            rom = self.active_rom
+            if len(event.args) > 0:
+                newpath = path.join(rom_path, " ".join(event.args))
+                if path.exists(newpath):
+                    rom = newpath
+                    self.em.kill()
+                    self.em = Popen([self.active_emu, rom])
+                    event.channel.msg("Emulator started with rom " + rom)
+                else:
+                    event.channel.msg("ROM does not exist")
+            else:
+                if isrunning(self.em) != None:
+                    self.em = Popen([self.active_emu, rom])
+                    event.channel.msg("Emulator initiated")
+                else:
+                    event.channel.msg("Emulator already running")
+
+    @bones.event.handler(trigger="emurestart")
+    def emurestart(self, event):
+        if event.user.nickname in mod_admins:
+            if isrunning(self.em) == None:
+                self.em.kill()
+            self.em = Popen([self.active_emu, self.active_rom])
+            event.channel.msg("Emulator restarted")
+
+    @bones.event.handler(trigger="emustop")
+    def emustop(self, event):
+        if event.user.nickname in mod_admins:
+            if isrunning(self.em) == None:
+                self.em.kill()
+                event.channel.msg("Emulator killed")
+            else:
+                event.channel.msg("Emulator not running")
+
+    @bones.event.handler(trigger="emudebug")
+    def emudebug(self, event):
+        if event.user.nickname in mod_admins:
+            if isrunning(self.em) == None:
+                event.channel.msg("Emulator running on PID " + str(self.em.pid))
+            else:
+                event.channel.msg("Emulator not running")
+
+    @bones.event.handler(trigger="emuset")
+    def emuset(self, event):
+        global active_emu, active_rom, input_enabled
+        if event.user.nickname in mod_admins:
+            success = True
+            if len(event.args) > 0:
+                
+                if event.args[0].lower() == "emu":
+                    if path.exists(path.join(emu_path, " ".join(event.args[1:]))):
+                        active_emu = event.args[1]
+                    else:
+                        event.channel.msg("Emulator does not exist, changes ignored")
+                        success = False
+
+                if event.args[0].lower() == "rom":
+                    if path.exists(path.join(rom_path, " ".join(event.args[1:]))):
+                        active_rom = event.args[1]
+                    else:
+                        event.channel.msg("ROM does not exist, changes ignored")
+                        success = False
+
+                if event.args[0].lower() == "input":
+                    if event.args[1].lower() == "true":
+                        input_enabled = True
+                    elif event.args[1].lower() == "false":
+                        input_enabled = False
+            
+            if success == True:
+                event.channel.msg("Changes applied successfully")
+
+class emuinput(Module):
     def __init__(self, *args, **kwargs):
         bones.bot.Module.__init__(self, *args, **kwargs)
+        global input_enabled
         self.mutedUsers = {}
         self.keyQueue = []
         #reactor.callLater(0.0, reactor.callInThread, self.keyAgent)
@@ -35,7 +148,7 @@ class IRCPlaysPokemon(bones.bot.Module):
     
     @bones.event.handler(event=bones.event.PrivmsgEvent)
     def parseMessage(self, event):
-        if event.msg.lower() in ["select", "start", "down", "up", "left", "right", "a", "b"]:
+        if input_enabled == True and event.msg.lower() in ["select", "start", "down", "up", "left", "right", "a", "b"]:
             key = event.msg.lower()
             PressKey(keys[key])
             time.sleep(keyDelay)
@@ -54,4 +167,3 @@ class IRCPlaysPokemon(bones.bot.Module):
                 time.sleep(keyDelay)
                 ReleaseKey(keys[key])
                 bones.bot.log.debug("Sent %s, %s" % (key, keys[key]))
-                
