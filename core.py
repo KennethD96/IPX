faf07f7597a24e1d6f38682e07ec37ce640acab1
Simen Lybekk <simen@lybekk.no>
2014-12-11 20:47:11 +0100

    Prepare ze workaround

--- core.py
+++ core.py
@@ -6,52 +6,53 @@ import bones.event
 
 
 class IPXCore(bones.bot.Module):
-    _config = None
-    _admins = []
-    _allow_all_ops = True
-    _op_mode = "o"
+    # Sometiems Python can be a big fucking pain in the ass. Workaround for
+    # not being able to change class attributes when working on an instance.
+    _ = {
+        "config": None,
+    }
 
     def __init__(self, **args):
         bones.bot.Module.__init__(self, **args)
 
-        if not self._config:
-            self._config = SafeConfigParser(
+        if not self._["config"]:
+            self._["config"] = SafeConfigParser(
                 defaults={  # Interpolation
                     "here": os.getcwd(),
                 },
                 allow_no_value=True,
             )
-            self._config_path = self.settings.get(
+            self._["config_path"] = self.settings.get(
                 section="IPX",
                 option="config",
                 default=os.path.join(os.getcwd(), "IPX.ini"),
             )
-            self._config.read(self._config_path)
+            self._["config"].read(self._["config_path"])
 
-        self._admins = self.config("auth", "admins", default="").split("\n")
-        self._admins = bones.bot.removeEmptyElementsFromList(self._admins)
+        self._["admins"] = self.config("auth", "admins", default="").split("\n")
+        self._["admins"] = bones.bot.removeEmptyElementsFromList(self._["admins"])
 
-        self._allow_all_ops = bool(self.config("auth", "allow_all_ops", default=True))
-        self._op_mode = self.config("auth", "op_mode", default="o")
-        self._require_op = bool(self.config("auth", "require_op", default=False))
+        self._["allow_all_ops"] = bool(self.config("auth", "allow_all_ops", default=True))
+        self._["op_mode"] = self.config("auth", "op_mode", default="o")
+        self._["require_op"] = bool(self.config("auth", "require_op", default=False))
 
     def config(self, section, option, default=None):
-        if not self._config.has_section(section):
+        if not self._["config"].has_section(section):
             return default
 
-        if not self._config.has_option(section, option):
+        if not self._["config"].has_option(section, option):
             return default
 
-        data = self._config.get(section, option)
+        data = self._["config"].get(section, option)
         return data if data else default
 
     def authUser(self, event):
-        if self._allow_all_ops or len(self._admins) == 0:
-            if event.user.nickname in event.channel.modes[self._op_mode]:
+        if self._["allow_all_ops"] or len(self._["admins"]) == 0:
+            if event.user.nickname in event.channel.modes[self._["op_mode"]]:
                 return True
         if event.user.nickname in self._admins:
-            if self._require_op:
-                if event.user.nickname in event.channel.modes[self._op_mode]:
+            if self._["require_op"]:
+                if event.user.nickname in event.channel.modes[self._["op_mode"]]:
                     return True
                 else:
                     self.log.warning("User is not OP in this channel.")
